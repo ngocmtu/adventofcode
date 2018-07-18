0caab33ed5943a654a8448a91730edc064d1147a
ngocmtu <ngocbeo196@gmail.com>
2018-07-18 15:10:47 -0500

    Update day4_2017.py

--- day4_2017.py
+++ day4_2017.py
@@ -3,7 +3,21 @@ import fileinput
 # Part 1
 # reads from STDIN
 count_part1 = 0
-for line in fileinput.input:
-    count += 1 if len(line.split()) == len(list(set(line.split()))) else 0
+for line in fileinput.input():
+    count_part1 += 1 if len(line.split()) == len(list(set(line.split()))) else 0
 
+# Part 2
+count_part2 = 0
+def identify_anagram(words):
+    for index,word in enumerate(words):
+        for each in words[index+1:]:
+            if word == each:
+                return True
+    return False
+
+for line in fileinput.input():
+    words = [sorted(list(word)) for word in line.split()]
+    count_part2 += 0 if identify_anagram(words) else 1
+    
 print(str(count_part1))
+print(str(count_part2))
