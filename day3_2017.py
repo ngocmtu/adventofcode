9cc36964bcf79ac88859bb81fce1e6417e4bf4ff
ngocmtu <ngocbeo196@gmail.com>
2018-07-18 14:27:14 -0500

    Update day3_2017.py

--- day3_2017.py
+++ day3_2017.py
@@ -1,5 +1,11 @@
 s = 347991
-def solve(s):
+
+# Part 1
+# Bottom right corner is the square of the outer length of the memory grid
+# If s is smaller than bottom right corner, s is on the layer of the grid with that length
+# the shortest route from outer layer to center is distance between the middle point of side and center
+# shortest route from s to middle point is distance between s and middle point
+def solve_part1(s):
   length = 1
   while length**2 <s:
     length+=2
@@ -19,85 +25,56 @@ def solve(s):
   s_to_halfway = s-halfway_point
   return halfway_to_1 + s_to_halfway if s_to_halfway > 0 else halfway_to_1 + s_to_halfway*(-1)
 
+
+# Part 2
+# The code below builds out the spiral memory using a dictionary with coordinates as keys and values as, well, values
 d={(0,0):1}
 
-# TODO
-# up left, right down, down right all need 4 inputs instead of just three
-
-def calc_next(x,y,i,direction):
+def calc_next(x,y):
   next_cord = (x,y)
-  print('next cord '+str(next_cord))
-  print('i '+str(i))
   
-  if x == 1 and y == 0:
-    d[next_cord] = 1
-  elif direction == 'left_up':
-    d[next_cord] = d[(next_cord[0],next_cord[1]-1)] + d[(next_cord[0]-1,next_cord[1])] + d[(next_cord[0]-1,next_cord[1]-1)] if i>0 else d[(next_cord[0]-1,next_cord[1]-1)] + d[(next_cord[0]-1,next_cord[1])]
-  elif direction == 'up_left':
-    d[next_cord] = d[(next_cord[0]-1,next_cord[1]-1)] + d[(next_cord[0],next_cord[1]-1)] + d[(next_cord[0],next_cord[1]-1)] if i>0 else d[(next_cord[0]-1,next_cord[1]-1)] + d[(next_cord[0],next_cord[1]-1)]
-  elif direction == 'right_down':
-    d[next_cord] = d[(next_cord[0]-1,next_cord[1]-1)] + d[(next_cord[0]-1,next_cord[1])] + d[(next_cord[0]-1,next_cord[1]+1)] if i>0 else d[(next_cord[0]-1,next_cord[1]-1)] + d[(next_cord[0]-1,next_cord[1])]
-  elif direction == 'down_right':
-    d[next_cord] = d[(next_cord[0]-1,next_cord[1]-1)] + d[(next_cord[0],next_cord[1]-1)] + d[(next_cord[0]+1,next_cord[1]-1)] if i>0 else d[(next_cord[0],next_cord[1]-1)] + d[(next_cord[0]+1,next_cord[1]-1)]
-  elif direction == 'corner':
-    d[next_cord] = d[(next_cord[0]-1,next_cord[1]+1)] + d[(next_cord[0],next_cord[1]+1)] + d[(next_cord[0]-1,next_cord[1])]
-  print(str(d))
-  print('next_cord after conditional '+str(next_cord))
-  if d[next_cord] > s:
-    return d[next_cord]
-  else:
-    return None
+  # cord of surrounding values
+  # if there's a surrounding value, add that the value of the position in examination
+  surroundings = [(x,y+1),(x+1,y),(x-1,y),(x,y-1),(x+1,y+1),(x-1,y-1),(x-1,y+1),(x+1,y-1)]
+  existing_cords = d.keys()
+  d[next_cord] = sum([d[each] if each in existing_cords else 0 for each in surroundings])
+  return d[next_cord] if d[next_cord] > s else None
 
 def solve_2(s):
-  answer_not_found = True
-  base_cord_x = 0
-  base_cord_y = 0
-  length=1
-  while answer_not_found:
-    length += 2
+  base_x = 0
+  base_y = 0
+  length=0
+  while True:
+    length += 1
     # going up left side
-    base_cord_x += 1
-    for i in range(length-2):
-      base_cord_y += i
-      print('base y '+str(base_cord_y))
-      print('i step '+str(i))
-      result = calc_next(base_cord_x,base_cord_y,i,'left_up')
-      if not result is None:
-        answer_not_found = False
-        return result
+    base_x += 1
+    for i in range(length):
+      result = calc_next(base_x,base_y)
+      if not result is None: return result
+      base_y += 1
     
+    length+=1
     # going left up side
-    base_cord_y += 1
-    for i in range(length-1):
-      base_cord_x -= i
-      calc_next(base_cord_x,base_cord_y,i,'up_left')
-      if not result is None:
-        answer_not_found = False
-        return result
+    for i in range(length):
+      result = calc_next(base_x,base_y)
+      if not result is None: return result
+      base_x -= 1
     
     # going down right side
-    base_cord_x -= 1
-    for i in range(length-1):
-      base_cord_y -= i
-      calc_next(base_cord_x,base_cord_y,i,'right_down')
-      if not result is None:
-        answer_not_found = False
-        return result
+    for i in range(length):
+      result = calc_next(base_x,base_y)
+      if not result is None: return result
+      base_y -= 1
         
     # going right down side
-    base_cord_y -=1
-    for i in range(length-1):
-      base_cord_x += i
-      calc_next(base_cord_x,base_cord_y,i,'down_right')
-      if not result is None:
-        answer_not_found = False
-        return result
+    for i in range(length):
+      result = calc_next(base_x,base_y)
+      if not result is None: return result
+      base_x += 1
         
     # bottom right corner needs separate calculation
-    base_cord_x += 1
-    calc_next(base_cord_x,base_cord_y,0,'corner')
-    if not result is None:
-      answer_not_found = False
-      return result
-  
-print(solve(s))
+    result = calc_next(base_x,base_y)
+    if not result is None: return result
+      
+print(solve_part1(s))
+print(solve_part2(s))
