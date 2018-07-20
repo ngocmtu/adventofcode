c0a79d0fda66a1eb4e47e0987e9ff1d8c534ee75
ngocmtu <ngocbeo196@gmail.com>
2018-07-20 15:42:05 -0500

    Update day5_2017.py

--- day5_2017.py
+++ day5_2017.py
@@ -14,5 +14,20 @@ def solve_part1(s):
 		step = s[cur_pos]
 		new_pos = cur_pos + step
 		s[cur_pos] += 1
-    
+
+def solve_part2(s):
+	new_pos = 0
+	cur_pos = 0
+	count = 0
+	while True:
+		if new_pos >= len(s) or new_pos < 0:
+			return count
+		else:
+			count += 1
+		cur_pos = new_pos
+		step = s[cur_pos]
+		new_pos = cur_pos + step
+		s[cur_pos] += 1 if s[cur_pos] <3 else -1
+		
 print(str(solve_part1(s)))
+print(str(solve_part2(s)))
